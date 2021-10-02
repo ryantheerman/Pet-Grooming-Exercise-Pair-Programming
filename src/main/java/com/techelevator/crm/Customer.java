c6a51ff4943988fdccc5fed4fa388c205791cab4
YOUR NAME GOES HERE <rwtheerman@gmail.com>
2021-10-02 13:42:52 -0400

    wrote customer class

---
+++ src/main/java/com/techelevator/crm/Customer.java
@@ -0,0 +1,37 @@
+package com.techelevator.crm;
+
+import com.techelevator.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Customer extends Person {
+
+    private String phoneNumber;
+    private List<String> pets = new ArrayList<>();
+
+    public Customer(String firstName, String lastName, String phoneNumber) {
+        super(firstName, lastName);
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Customer(String firstName, String lastName) {
+        super(firstName, lastName);
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public List<String> getPets() {
+        return pets;
+    }
+
+    public void setPets(List<String> pets) {
+        this.pets = pets;
+    }
+}
